6536e2b77c3bdfb10b670f8afc9a9366a7148156
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-03-20 19:21:50 +0100

    add project

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react,
+      'jsx-a11y': jsx - a11y,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/role-has-required-aria-props": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+]
---
+++ src/App/App.jsx
@@ -0,0 +1,11 @@
+import ListItems from "../LisItems/LisItems"
+function App() {
+  return (
+    <>
+      <h1>Список элементов</h1>
+      <ListItems />
+    </>
+  )
+}
+
+export default App
